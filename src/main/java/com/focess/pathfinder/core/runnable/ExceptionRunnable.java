cf8a47d38c3f924f5748ab26ac06eafa76f371f7
4125412435 <3487457366@qq.com>
2020-03-09 16:55:30 +0800

    just test

--- src/main/java/com/focess/pathfinder/core/runnable/ExceptionRunnable.java
+++ src/main/java/com/focess/pathfinder/core/runnable/ExceptionRunnable.java
@@ -3,6 +3,9 @@ package com.focess.pathfinder.core.runnable;
 import com.focess.pathfinder.core.exception.ExceptionCatcher;
 
 public class ExceptionRunnable implements Runnable {
+    public void a(){
+        System.out.println("f**k");
+    }
 
     @Override
     public void run() {
