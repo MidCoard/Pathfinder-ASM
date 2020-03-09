aea330df145681a9d3e19e7326f35f614d9ab0a9
MidCoard <2624646185@qq.com>
2020-03-09 16:48:35 +0800

    add goal dump builder....

--- src/main/java/com/focess/pathfinder/core/PathfinderPlugin.java
+++ src/main/java/com/focess/pathfinder/core/PathfinderPlugin.java
@@ -1,10 +1,15 @@
 package com.focess.pathfinder.core;
 
+import com.focess.pathfinder.core.builder.PathfinderClassLoader;
+import com.focess.pathfinder.core.goal.NMSGoalDump;
 import com.focess.pathfinder.core.runnable.ExceptionRunnable;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PathfinderPlugin extends JavaPlugin {
+    static {
+        PathfinderClassLoader.loadClasses();
+    }
 
     public void onEnable() {
         this.getServer().getScheduler().runTaskTimerAsynchronously(this, new ExceptionRunnable(),0L,20L);
