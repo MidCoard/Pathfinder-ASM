0c378de686a1c6b520161f5723ebe3b0dc2e0bf9
4125412435 <3487457366@qq.com>
2020-03-14 09:05:41 +0800

    Navigation

---
+++ src/main/java/com/focess/pathfinder/core/exception/NavigationUnMatch.java
@@ -0,0 +1,7 @@
+package com.focess.pathfinder.core.exception;
+
+public class NavigationUnMatch extends Exception{
+    public NavigationUnMatch(String str){
+        super(str);
+    }
+}
---
+++ src/main/java/com/focess/pathfinder/core/navigation/NMSNavigation.java
@@ -0,0 +1,35 @@
+package com.focess.pathfinder.core.navigation;
+
+import com.focess.pathfinder.core.exception.NavigationUnMatch;
+import com.focess.pathfinder.core.util.NMSManager;
+import com.focess.pathfinder.goal.entity.FocessEntity;
+import org.bukkit.entity.Entity;
+
+import java.lang.reflect.Method;
+
+public abstract class NMSNavigation {
+    private Class<?> navigation;
+    private FocessEntity focessEntity;
+    public NMSNavigation(FocessEntity entity) throws Exception {
+        this.focessEntity = entity;
+        Adapt(entity);
+    }
+    private void Adapt(FocessEntity entity) throws Exception {
+        Entity bukkit = entity.getBukkitEntity();
+        Class<?> craftClass;
+        if ((craftClass = bukkit.getClass()).isAssignableFrom(NMSManager.getCraftClass("CraftEntity"))) {
+            Method getHandle = NMSManager.getMethod(craftClass, "getHandle");
+            Class<?> nmsEntity = getHandle.invoke(craftClass).getClass();
+            if (nmsEntity.isAssignableFrom(NMSManager.getNMSClass("EntityInsentient", true))) {
+                Method getNavigation = NMSManager.getMethod(nmsEntity, "getNavigation");
+                this.navigation = (Class<?>) getNavigation.invoke(nmsEntity);
+                return;
+            }
+        }
+        throw new NavigationUnMatch("Could not match the navigation from this entity");
+    }
+
+    public enum NavigationType{
+        Fly,Guardian,Normal
+    }
+}
